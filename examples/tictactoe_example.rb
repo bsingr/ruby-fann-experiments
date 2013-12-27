a0365490a3b5ceb7136ab5e091ed3acceffa2899
Jens Bissinger <mail@jens-bissinger.de>
2013-12-27 20:08:21 +0100

    Compare ANN output to calculated output for tictactoe

--- examples/tictactoe_example.rb
+++ examples/tictactoe_example.rb
@@ -5,17 +5,26 @@ require 'example_env'
 NAME = ARGV.first || 'tictactoe'
 
 data = training_data(NAME)
-p data[:inputs].last
-p data[:outputs].last
 train = build_train_data(data)
-fann = RubyFann::Standard.new(:num_inputs=>9,
-                              :hidden_neurons=>[12,6,6,6,6,6,6],
+neural_net = RubyFann::Standard.new(:num_inputs=>9,
+                              :hidden_neurons=>[3,3],
                               :num_outputs=>1)
-fann.train_on_data(train, 300, 100, 0.1) # max epochs, errors between reports, desired mean-squared-error
+neural_net.train_on_data(train, 300000, 1000, 0.1) # max epochs, errors between reports, desired mean-squared-error
 
-p fann.run([0,0,-1, 0,-1,-1, 1,1,1]) # 1
-p fann.run([0,0,-1, 0,-1,0, 1,1,1]) # 1
-p fann.run([0,0,-1, 0,-1,-1, 1,0,1]) # 0
-p fann.run([0,0,-1, 0,-1,-1, 1,1,1]) # 1
-p fann.run([0,0,1, -1,-1,-1, 0,1,1]) # -1
-p fann.run([0,1,1, -1,-1,-1, -1,1,1]) # -1
+examples = []
+examples << [0,0,-1, 0,-1,0, 1,0,0]   # 0
+examples << [0,0,-1, 0,-1,-1, 1,1,1]  # 1
+examples << [0,0,-1, 0,-1,0, 1,1,1]   # 1
+examples << [0,0,-1, 0,-1,-1, 1,0,1]  # 0
+examples << [0,0,-1, 0,-1,-1, 1,1,1]  # 1
+examples << [0,0,1, -1,-1,-1, 0,1,1]  # -1
+examples << [0,1,1, -1,-1,-1, -1,1,1] # -1
+examples << [0,1,-1, 1,0,-1, 0,0,-1]  # -1
+examples.each do |e|
+  raise "Invalid sum of #{e.inspect}" if e.inject(:+) > 1 || e.inject(:+) < -1 
+  s = TTT::Situation.new(e)
+  printf "%02f  -  %02f\n", s.winner, neural_net.run(e).first
+end
+
+neurotica = RubyFann::Neurotica.new
+neurotica.graph(neural_net, "tictactoe.png")
