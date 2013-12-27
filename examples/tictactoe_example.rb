f9bbfccfa58da4c133c9b1e0e4e20e4e4e5ef846
Jens Bissinger <mail@jens-bissinger.de>
2013-12-27 16:47:04 +0100

    Generate possible tictactoe game scenarios and identify the winner

--- examples/tictactoe_example.rb
+++ examples/tictactoe_example.rb
@@ -28,3 +28,64 @@ require 'example_env'
 
 # output
 # h0
+
+permutations = []
+(3**9).times do |idx|
+  permutation = []
+  permutation.push (idx / (3**8)) % (3**8) % (3**7) % (3**6) % (3**5) % (3**4) % (3**3) % (3**2) % (3**1)
+  permutation.push (idx / (3**7)) % (3**7) % (3**6) % (3**5) % (3**4) % (3**3) % (3**2) % (3**1)
+  permutation.push (idx / (3**6)) % (3**6) % (3**5) % (3**4) % (3**3) % (3**2) % (3**1)
+  permutation.push (idx / (3**5)) % (3**5) % (3**4) % (3**3) % (3**2) % (3**1)
+  permutation.push (idx / (3**4)) % (3**4) % (3**3) % (3**2) % (3**1)
+  permutation.push (idx / (3**3)) % (3**3) % (3**2) % (3**1)
+  permutation.push (idx / (3**2)) % (3**2) % (3**1)
+  permutation.push (idx / (3**1)) % (3**1)
+  permutation.push (idx / (3**0)) % (3**1)
+  permutations << permutation
+end
+
+#puts permutations[0..33].map{|s| s.join(' ')}.join("\n")
+
+normalized_permutations = permutations.map do |p|
+  p.map do |value|
+    value - 1
+  end
+end
+
+# p normalized_permutations[1]
+
+# only keep permutations were each player made equal number of moves
+balanced_permutations = normalized_permutations.find_all do |p|
+  sum = p.inject &:+
+  sum >= -1 && sum <= 1
+end
+
+# p filtered_permutation[1]
+
+# calculate winner
+# and only keep permutations with one clear winner (not more than one)
+priced_permutations = balanced_permutations.map do |p|
+  strikes = [p[0]+p[1]+p[2], # row 1
+             p[3]+p[4]+p[5], # row 2
+             p[6]+p[7]+p[8], # row 3
+             p[0]+p[3]+p[6], # col 1
+             p[1]+p[4]+p[7], # col 2
+             p[2]+p[5]+p[8], # col 3
+             p[0]+p[4]+p[8], # diag 1
+             p[2]+p[4]+p[6]] # diag 2
+  minus_winning_strikes = strikes.find_all{|s| s == -3}
+  plus_winning_strikes = strikes.find_all{|s| s == 3}
+  if (minus_winning_strikes.size + plus_winning_strikes.size) <= 1
+    winner = if minus_winning_strikes.size == 1
+      -1
+    elsif plus_winning_strikes.size == 1
+      1
+    else
+      0
+    end
+    [p, winner]
+  else 
+    # noop, too much winners
+  end
+end.compact
+
