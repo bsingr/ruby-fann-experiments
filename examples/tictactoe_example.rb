1239430fd0218f371c7941b8fc41de1496e63222
Jens Bissinger <mail@jens-bissinger.de>
2015-05-14 14:51:01 +0200

    Update.

--- examples/tictactoe_example.rb
+++ examples/tictactoe_example.rb
@@ -7,9 +7,9 @@ NAME = ARGV.first || 'tictactoe'
 data = training_data(NAME)
 train = build_train_data(data)
 neural_net = RubyFann::Standard.new(:num_inputs=>9,
-                              :hidden_neurons=>[3,3],
-                              :num_outputs=>1)
-neural_net.train_on_data(train, 300000, 1000, 0.1) # max epochs, errors between reports, desired mean-squared-error
+                                    :hidden_neurons=>[9,9,9],
+                                    :num_outputs=>1)
+neural_net.train_on_data(train, 500, 50, 0.1) # max epochs, errors between reports, desired mean-squared-error
 
 examples = []
 examples << [0,0,-1, 0,-1,0, 1,0,0]   # 0
